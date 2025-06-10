c0e33ee5a91e0d683f96c9e51b2d48edc4e98931
myramaurice1234 <84265939+MyraOphelia@users.noreply.github.com>
2025-06-10 16:13:10 +0800

    fragment02 error

--- app/src/main/java/com/ebookfrenzy/labtestmyralast/Fragment.kt
+++ app/src/main/java/com/ebookfrenzy/labtestmyralast/Fragment.kt
@@ -1,20 +1,24 @@
-package com.ebookfrenzy.labtestmyralast
+class OneImageFragment : Fragment() {
 
-import android.os.Bundle
+    companion object {
+        private const val ARG_IMAGE = "image_res"
 
-open class Fragment {
-    class OneImageFragment : Fragment() {
-
-        companion object {
-            private const val ARG_IMAGE = "image_res"
-
-            fun newInstance(imageResId: Int): OneImageFragment {
-                val fragment = OneImageFragment()
-                val args = Bundle()
-                args.putInt(ARG_IMAGE, imageResId)
-                fragment.arguments = args
-                return fragment
-            }
+        fun newInstance(imageResId: Int): OneImageFragment {
+            val fragment = OneImageFragment()
+            val args = Bundle()
+            args.putInt(ARG_IMAGE, imageResId)
+            fragment.arguments = args
+            return fragment
         }
-}
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_one_image, container, false)
+        val imageView: ImageView = view.findViewById(R.id.imageViewOne)
+        val resId = arguments?.getInt(ARG_IMAGE) ?: R.drawable.kitten
+        imageView.setImageResource(resId)
+        return view
+    }
+}
--- app/src/main/java/com/ebookfrenzy/labtestmyralast/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/labtestmyralast/MainActivity.kt
@@ -23,8 +23,7 @@ class MainActivity : AppCompatActivity() {
         imageView.setImageResource(R.drawable.kitten)
 
 
-
-            override fun onCreateView(
+        override fun onCreateView(
                 inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
             ): View? {
                 val view = inflater.inflate(R.layout.fragment_one_image, container, false)
